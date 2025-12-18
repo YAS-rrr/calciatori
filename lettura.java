715dad504af5cde41e4fe4948482b2c19c804a73
YAS-rrr <shallowpiller@gmail.com>
2025-12-18 07:53:04 +0000

    Rimozione del codice non utilizzato nella classe lettura

--- lettura.java
+++ lettura.java
@@ -4,38 +4,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class lettura {
 
-    reader = CSVReader("data.csv")
-    reader.read()
-
-    riga = reader.data[0]  
-
-
-        
-        public void readData(riga[] letturaArray) {
-
-        try{
-           
-            File file = new File("calciatori.csv");
-            Scanner scanner = new Scanner(file);
-            
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                String[] values = line.split(",");
-                
-            
-                for (String value : values) {
-                    System.out.print(value + " ");
-                }
-        
-        }
-
-
-           
-        } 
-        
-        catch (FileNotFoundException e) {
-            System.out.println("File non trovato: " + e.getMessage());
-        }
+   
         }
 
        
