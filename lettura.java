16e64a7624e7d4662de98b2c5eeffaa59447f0ec
YAS-rrr <shallowpiller@gmail.com>
2025-12-18 07:59:47 +0000

    Correzione della classe lettura: aggiunta delle proprietà cognome ed eta, e implementazione del metodo toString

--- lettura.java
+++ lettura.java
@@ -5,10 +5,23 @@ import java.util.Scanner;
 public class lettura {
 
    
-        }
+ 
+    private String cognome;
+    private int eta;
+    
+
+    public Persona(String cognome, int eta) {
+        this.cognome = cognome;
+        this.eta = eta;
+        
+    }
+
+    @Override
+    public String toString() {
+        return nome + " - " + eta + " - " + citta;
+    }
+}
+
 
        
-            
-         
-    
-}
+        
